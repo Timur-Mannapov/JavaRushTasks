c4d8a67973679b5a6fde5784b106245b91745976
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-05 00:15:59 +0300

    add Solution JavaSyntax task 0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 3126;
+        System.out.println(x-8);
+    }
+}

9f3a41fdb155c96d276a47408d1077756c2bd548
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-05 13:39:12 +0300

    add Solution JavaSyntax task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "x", phrase = "y", line = "z", text = "t";
+
+    }
+}

3d7a0acefe2f9b4fdb66eb1ed79082e358c62bd2
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-11 14:21:17 +0300

    add Solution JavaSyntax task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0404;
+
+/*
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int q = 0;
+        while (q < 5) {
+            int n = 0;
+            while (n < 10) {
+                System.out.print("Q");
+                n++;
+            }
+            System.out.println();
+            q++;
+        }
+
+    }
+}

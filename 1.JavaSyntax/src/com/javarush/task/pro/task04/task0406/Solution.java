7e217a8a915e97413d0a1578a207f8c564d03a36
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-11 16:40:52 +0300

    add Solution JavaSyntax task 0405

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task04.task0406;
+
+import java.util.Scanner;
+
+/* 
+Показываем, что получаем
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        while (true) {
+            String pass = console.nextLine();
+            if (pass.equals("enough")) {
+                break;
+            } else {
+                System.out.println(pass);
+            }
+
+        }
+
+    }
+}

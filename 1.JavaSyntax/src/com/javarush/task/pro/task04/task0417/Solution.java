7cdddce765f106f350ee5f1832b86c223ceaa3cf
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-15 19:10:43 +0300

    add Solution JavaSyntax task 0417

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0417;
+
+import java.util.Scanner;
+
+/* 
+Скорость ветра
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ms = scanner.nextInt();
+        int km = (int)Math.round(ms*3.6);
+        System.out.println(km);
+    }
+}

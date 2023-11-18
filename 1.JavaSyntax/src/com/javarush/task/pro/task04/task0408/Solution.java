780d57d564f823dccdd410927d6c85131053a841
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-18 21:25:41 +0300

    add Solution JavaSyntax task 0408

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0408;
+
+import java.util.Scanner;
+
+/* 
+Максимум из введенных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        while (console.hasNextInt()) {
+            int number = console.nextInt();
+            if (max < number && number%2 == 0) {
+                max = number;
+            }
+
+        }
+        System.out.println(max);
+    }
+}

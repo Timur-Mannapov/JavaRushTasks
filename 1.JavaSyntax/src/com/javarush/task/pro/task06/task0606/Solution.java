9335dc5ed81e37b71648719a098e6b799543f5b3
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-22 21:16:18 +0300

    add Solution JavaSyntax task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(int first){
+
+    }
+    public static void universalMethod(int[] first){
+
+    }
+    public static void universalMethod(int[][] first){
+
+    }
+    public static void universalMethod(int[][][] first){
+
+    }
+    public static void universalMethod(double first){
+
+    }
+    public static void universalMethod(byte first){
+
+    }
+    public static void universalMethod(long first){
+
+    }
+    public static void universalMethod(String first){
+
+    }
+    public static void universalMethod(int first, int second){
+
+    }
+
+}

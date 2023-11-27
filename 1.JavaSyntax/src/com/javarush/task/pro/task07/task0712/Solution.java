3e258c4a9989cfd0c26c8c02d88670a8c85205b1
Timur Mannapov <bla.blacarr@yandex.ru>
2023-11-27 15:29:58 +0300

    add Solution JavaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String a = new String("");
+        String b = new String("console");
+        String c = new String("");
+        String d = new String("");
+        String e = new String("");
+        String j = new String("");
+        String o = new String("");
+        String p = new String("");
+        String g = new String("");
+        String z = new String("");
+        Scanner integer = new Scanner(System.in);
+        int[] q = new int[1];
+        int[] w = new int[1];
+        int[] y = new int[1];
+        int[] r = new int[1];
+        int[] t = new int[1];
+    }
+}

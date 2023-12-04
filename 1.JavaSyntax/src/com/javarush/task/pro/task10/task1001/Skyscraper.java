21cdd4a0bdc5f13155a4a8c478cc778665b2d71a
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-04 16:59:56 +0300

    add Solution JavaSyntax task 1001

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1001;
+
+/* 
+Создадим небоскреб
+*/
+
+public class Skyscraper {
+
+    public Skyscraper() {
+        String sky = "Небоскреб построен.";
+        System.out.println(sky);
+    }
+
+    public static void main(String[] args) {
+        Skyscraper ky = new Skyscraper();
+    }
+}

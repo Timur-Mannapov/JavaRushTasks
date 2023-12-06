263f5db58cb22ad8ba00a75b6fe7393005949d20
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-06 20:30:49 +0300

    add Solution JavaSyntax task 1008

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1008/Programmer.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task10.task1008;
+
+/* 
+Зарплата
+*/
+
+public class Programmer {
+    private int salary = 1000;
+
+    public void Prog(){
+        this.salary = salary;
+    }
+    public int getSalary() {
+            return salary;
+    }
+
+    public void setSalary(int salary) {
+       if (salary > this.salary) {
+           this.salary = salary;
+       }
+    }
+}

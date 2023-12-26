392f32da497d1299c7f580bff954ae478b225801
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-26 19:04:09 +0300

    add Solution JavaSyntax task 1312

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task13.task1312;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/* 
+ArrayList vs HashMap
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getProgrammingLanguages());
+    }
+
+    public static HashMap<Integer, String> getProgrammingLanguages() {
+        HashMap<Integer, String> map = new HashMap<>();
+
+
+        ArrayList<String> programmingLanguages = new ArrayList<>();
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Kotlin");
+        programmingLanguages.add("Go");
+        programmingLanguages.add("Javascript");
+        programmingLanguages.add("Typescript");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("PHP");
+        programmingLanguages.add("C++");
+        for (String element : programmingLanguages) {
+            map.put(programmingLanguages.indexOf(element), element);
+        }
+        return map;
+    }
+
+}

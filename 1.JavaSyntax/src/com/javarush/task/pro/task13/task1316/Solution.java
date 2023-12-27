af3452bffce38f0b1efdbb1b2f4d06b0313c4cbd
Timur Mannapov <bla.blacarr@yandex.ru>
2023-12-27 21:36:31 +0300

    add Solution JavaSyntax task 1316

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] javarushQuests = JavarushQuest.values();
+        for (JavarushQuest quest : javarushQuests) {
+
+            System.out.println(quest.ordinal());
+        }
+    }
+}

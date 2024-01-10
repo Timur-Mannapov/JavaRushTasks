66dfff10c889e36b103ed1db5161a446e4945355
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-10 22:34:51 +0300

    add Solution JavaSyntax task 1515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task15.task1515;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Абсолютный путь
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        boolean abs = Path.of(str).isAbsolute();
+        if (abs == true) {
+            System.out.println(abs);
+        } else {
+            Path path = Path.of(str).toAbsolutePath();
+            System.out.println(path);
+        }
+        }
+    }
+
+

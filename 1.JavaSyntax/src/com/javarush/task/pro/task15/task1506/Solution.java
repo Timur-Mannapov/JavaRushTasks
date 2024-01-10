9468ba1e93ba5da13262fb482970fe11bca060d6
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-10 22:19:52 +0300

    add Solution JavaSyntax task 1506

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task15.task1506;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Фейсконтроль
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (Scanner scanner = new Scanner(System.in)) {
+             List<String> input = Files.readAllLines(Path.of(scanner.nextLine()));
+             for (String str : input) {
+                 System.out.println(str);
+             }
+        }
+    }
+}
+

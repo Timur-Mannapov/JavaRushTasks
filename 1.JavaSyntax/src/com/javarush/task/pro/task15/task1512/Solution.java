b8aaa7588de0a32a7b6c808015eac5152d65f2e1
Timur Mannapov <bla.blacarr@yandex.ru>
2024-01-12 21:35:28 +0300

    add Solution JavaSyntax task 1512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1512/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task15.task1512;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* 
+Задом наперед
+*/
+
+public class Solution {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+//        String reversedResult = new StringBuilder(result).reverse().toString();
+//        System.out.println(reversedResult);
+
+//        StringBuilder resultReverse = new StringBuilder(result);
+//        String newResult = String.valueOf(resultReverse.reverse());
+//        System.out.println(newResult);
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}

1f4ea199eb7af8c4b6b136d665376579b369f783
Timur Mannapov <bla.blacarr@yandex.ru>
2024-02-20 18:11:17 +0300

    add Solution JavaCore task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print((Integer) 2);
+        print(2);
+    }
+    public static void print(int a) {
+
+    }
+    public static void print(Integer a) {
+
+    }
+}

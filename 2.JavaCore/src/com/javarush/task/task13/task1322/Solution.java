9807c52b8d45efb924b54d4fee823c1a9023653b
Timur Mannapov <bla.blacarr@yandex.ru>
2024-02-25 13:31:29 +0300

    add Solution JavaCore task 1322

--- 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1322/Solution.java
@@ -7,11 +7,17 @@ package com.javarush.task.task13.task1322;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-//        SimpleObject<String> stringObject = new StringObject();
+        SimpleObject<String> stringObject = new StringObject();
     }
 
     interface SimpleObject<T> {
         SimpleObject<T> getInstance();
+    }
+    public static class StringObject implements SimpleObject<String> {
+        @Override
+        public SimpleObject<String> getInstance() {
+            return null;
+        }
     }
 
 }

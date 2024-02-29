dff27fc32be483d3f8d6c661cfaea02b1800d5fe
Timur Mannapov <bla.blacarr@yandex.ru>
2024-02-29 18:36:56 +0300

    add Solution JavaCore task 1409

--- 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1409/Solution.java
@@ -6,10 +6,13 @@ package com.javarush.task.task14.task1409;
 
 public class Solution {
     public static void main(String[] args) {
-//        println(new WaterBridge());
-//        println(new SuspensionBridge());
+        println(new WaterBridge());
+        println(new SuspensionBridge());
     }
 
-    //add println method here
+
+    public static void println(Bridge bridge){
+        System.out.println(bridge.getCarsCount());
+    }
 }
 

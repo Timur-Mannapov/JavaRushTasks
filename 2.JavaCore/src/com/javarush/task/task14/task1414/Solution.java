b91c41d87f4f89585e3f85b9d2dadbb5f8462db4
Timur Mannapov <bla.blacarr@yandex.ru>
2024-03-03 21:21:18 +0300

    add Solution JavaCore task 1414

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -3,34 +3,39 @@ package com.javarush.task.task14.task1414;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/* 
-MovieFactory
-*/
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //ввести с консоли несколько ключей (строк), пункт 7
-
-        /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
-        */
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (reader.ready()) {
+            String console = reader.readLine();
+            if ("cartoon".equals(console)) {
+                Movie movie = MovieFactory.getMovie(console);
+                System.out.println(movie.getClass().getSimpleName());
+            } else if ("thriller".equals(console)) {
+                Movie movie = MovieFactory.getMovie(console);
+                System.out.println(movie.getClass().getSimpleName());
+            } else if ("soapOpera".equals(console)) {
+                Movie movie = MovieFactory.getMovie(console);
+                System.out.println(movie.getClass().getSimpleName());
+            } else {
+                Movie movie = MovieFactory.getMovie(console);
+                break;
+            }
+        }
     }
 
     static class MovieFactory {
 
         static Movie getMovie(String key) {
             Movie movie = null;
-
-            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
             if ("soapOpera".equals(key)) {
                 movie = new SoapOpera();
+            } else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            } else if ("thriller".equals(key)) {
+                movie = new Thriller();
             }
-
-            //напишите тут ваш код, пункты 5,6
-
             return movie;
         }
     }
@@ -41,5 +46,10 @@ public class Solution {
     static class SoapOpera extends Movie {
     }
 
-    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie {
+
+    }
+    static class Thriller extends Movie {
+
+    }
 }

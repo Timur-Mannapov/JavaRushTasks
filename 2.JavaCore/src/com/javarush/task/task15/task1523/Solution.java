584700d38d82e6337841ea04293f9bc343dad8dc
Timur Mannapov <bla.blacarr@yandex.ru>
2024-03-18 22:21:05 +0300

    add Solution JavaCore task 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    Solution() {
+    }
+    protected Solution(int n) {
+    }
+    private Solution(double n) {
+    }
+    public Solution(String string) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+     SubSolution() {
+    }
+
+    public SubSolution(int n) {
+        super(n);
+    }
+
+    protected SubSolution(String string) {
+        super(string);
+    }
+}

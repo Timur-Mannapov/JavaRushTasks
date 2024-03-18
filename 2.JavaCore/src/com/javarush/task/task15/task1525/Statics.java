36fbe0ae5807e829877e12fd4054d60db710d720
Timur Mannapov <bla.blacarr@yandex.ru>
2024-03-18 22:21:57 +0300

    add Solution JavaCore task 1525

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -1,14 +1,5 @@
 package com.javarush.task.task15.task1525;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-//
-//public class Statics {
-////   try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-////    FileReader fileReader = new FileReader(Path.of(reader))) {
-//
-//    }
-//    public static String FILE_NAME = /* add the path to your source file here */;
-////}
+public class Statics {
+    public static String FILE_NAME = "C:\\Users\\xargron\\Desktop\\Новый текстовый документ.txt";
+}

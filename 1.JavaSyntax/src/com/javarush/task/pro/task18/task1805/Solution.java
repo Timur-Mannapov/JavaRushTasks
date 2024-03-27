ec7675357955b762fddda4c153d4c86d7a6b5839
Timur Mannapov <bla.blacarr@yandex.ru>
2024-03-28 00:04:15 +0300

    add Solution JavaSyntax task 1805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1805/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task18.task1805;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/* 
+Прощание с лямбда-выражением
+*/
+
+class StringComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+
+        Collections.addAll(strings, "JavaRush", "Amigo", "Java Developer", "CodeGym");
+
+        sortStringsByLength(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static void sortStringsByLength(ArrayList<String> strings) {
+        Collections.sort(strings, new StringComparator());
+    }
+}

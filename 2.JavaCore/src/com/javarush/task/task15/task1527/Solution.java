a1670163ff5b1312ff753aac6b5684c61d6a81b0
Timur Mannapov <bla.blacarr@yandex.ru>
2024-03-30 23:20:04 +0300

    add Solution JavaCore task1527

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.task15.task1527;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /* 
 Парсер реквестов
@@ -12,24 +14,34 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
-        String string = url.substring(url.lastIndexOf("?")+1);
-        String[] split = string.split("&");
-        String[] afterSplit = new String[split.length*2];
-        for (int i = 0; i < split.length; i++) {
-            String[] thirdArray = split[i].split("=");
-            afterSplit[i*2] = thirdArray[0];
-            afterSplit[i*2+1] = thirdArray[1];
+        String[] words = url.substring(url.lastIndexOf("?") + 1).split("&");
+        for (String word : words) {
+            System.out.print(word.contains("=") ? word.substring(0, word.indexOf("=")) + " " : word + " ");
         }
-        for (String s : afterSplit) {
-            System.out.println(s);
+        System.out.println();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains("obj")) {
+                String string = words[i].substring(words[i].indexOf("=")+1);
+                try {
+                    double doub = Double.parseDouble(string);
+                    alert(doub);
+                } catch (NumberFormatException e) {
+                    alert(string);
+                }
+            }
         }
     }
 
-    public static void alert(double value) {
-        System.out.println("double: " + value);
-    }
 
-    public static void alert(String value) {
-        System.out.println("String: " + value);
-    }
-}
+            public static void alert ( double value){
+                System.out.println("double: " + value);
+            }
+
+            public static void alert (String value){
+                System.out.println("String: " + value);
+            }
+        }
+
+
+
+

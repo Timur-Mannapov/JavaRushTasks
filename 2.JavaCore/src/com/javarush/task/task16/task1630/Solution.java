552e11ed212d1b9e7445f6e519208e388f4dbee4
Timur Mannapov <bla.blacarr@yandex.ru>
2024-04-11 20:41:45 +0300

    add Solution JavaCore task 1630

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task16.task1630;
+
+import java.io.*;
+
+/* 
+Последовательный вывод файлов
+*/
+
+public class Solution {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    static {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+        } catch (IOException ignore) {
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        f.join();
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        private String fullFileName;
+        private StringBuilder builder = new StringBuilder();
+
+        @Override
+        public void run() {
+            try (BufferedReader reader = new BufferedReader(new FileReader(fullFileName))) {
+                while (reader.ready()) {
+                    String string = reader.readLine();
+                    builder.append(string + " ");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void setFileName(String fullFileName) {
+            this.fullFileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            return builder.toString();
+        }
+
+
+    }
+}

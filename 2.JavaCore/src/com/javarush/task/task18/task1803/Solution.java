1bd4a5cebec53efaa4ce6eb031043a615fe38d5c
Timur Mannapov <bla.blacarr@yandex.ru>
2024-04-27 20:20:25 +0300

    add Solution JavaCore task 1803

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task18.task1803;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /* 
 Самые частые байты
@@ -14,16 +14,35 @@ import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        byte[] bytes = Files.readAllBytes(Path.of(new Scanner(System.in).nextLine()));
+        HashMap<Byte, Integer> map = IntStream.range(0, bytes.length).boxed()
+                .collect(Collectors.toMap(i -> bytes[i], i -> 1, Integer::sum, HashMap::new));
+        map.entrySet().stream().filter(pair -> Objects.equals(pair.getValue(),Collections.max(map.values())))
+                .forEach(pair -> System.out.print(pair.getKey() + " "));
+    }
+
+
+
+
+
+
+
+
+
+    public static void main1(String[] args) throws Exception {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-             FileInputStream reader = new FileInputStream(bufferedReader.readLine())) {
+             FileInputStream input = new FileInputStream(bufferedReader.readLine())) {
             ArrayList<Integer> list = new ArrayList<>();
-            while (reader.available() > 0) {
-                list.add(reader.read());
+            while (input.available() > 0) {
+                list.add(input.read());
             }
-            int count = 0;
             Map<Integer, Integer> map = new HashMap<>();
             for (Integer i : list) {
-                map.put(i, count++);
+                if (map.containsKey(i)) {
+                    map.put(i, map.get(i) + 1);
+                } else {
+                    map.put(i, 1);
+                }
             }
             int maxValue = Collections.max(map.values());
             for (Map.Entry<Integer, Integer> entry : map.entrySet()) {

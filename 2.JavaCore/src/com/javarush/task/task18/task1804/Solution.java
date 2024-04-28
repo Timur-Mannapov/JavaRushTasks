87f632a6eb3a78459c125c4f5eaa575f5e867f76
Timur Mannapov <bla.blacarr@yandex.ru>
2024-04-28 22:48:28 +0300

    add Solution JavaCore task 1804

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -3,9 +3,7 @@ package com.javarush.task.task18.task1804;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /* 
 Самые редкие байты
@@ -20,11 +18,20 @@ public class Solution {
                 list.add(reader.read());
             }
             Map<Integer, Integer> map = new TreeMap<>();
-            int count = 0;
             for (Integer i : list) {
-                map.put(i, count++);
+                if (map.containsKey(i)) {
+                    map.put(i, map.get(i) + 1);
+                } else {
+                    map.put(i, 1);
+                }
             }
-            System.out.println(map.get(map.size()));
+            int min = Collections.min(map.values());
+            Set<Map.Entry<Integer, Integer>> set = map.entrySet();
+            for (Map.Entry<Integer, Integer> entry : set) {
+                if (entry.getValue() == min) {
+                    System.out.print(entry.getKey() + " ");
+                }
+            }
         }
     }
 }

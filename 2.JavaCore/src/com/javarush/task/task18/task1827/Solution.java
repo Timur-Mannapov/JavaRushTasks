76e9ee9fc51912aba76114651503f1aea6a9217d
Timur Mannapov <bla.blacarr@yandex.ru>
2024-05-07 22:46:53 +0300

    add Solution JavaCore task 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task18.task1827;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /* 
 Прайсы
@@ -12,19 +10,28 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             String fileName = bufferedReader.readLine();
-            FileReader reader = new FileReader(fileName);
-            FileWriter writer = new FileWriter(fileName);
-            int ID = 0;
-            if (args[0].equals("-c")) {
-
-                String productName = String.format("%30s", args[1]);
-                if (productName.equals("+\\S")) {
-                    ++ID;
-                    String price = String.format("%8s", args[2]);
-                    String quantity = String.format("%4s", args[3]);
-                    writer.write(ID + productName + price + quantity);
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            FileWriter writer = new FileWriter(fileName, true);
+            int maxID = 0;
+            String str;
+            while (reader.ready()) {
+                str = reader.readLine();
+                int temp = Integer.parseInt(str.substring(0,8).trim());
+                if (maxID < temp) {
+                    maxID = temp;
                 }
             }
+            reader.close();
+
+            if (args.length > 0 && args[0].equals("-c")) {
+                String productName = String.format("%-30s", args[1]);
+                String price = String.format("%-8s", args[2]);
+                String quantity = String.format("%-4s", args[3]);
+                maxID++;
+                writer.write(String.format("\n%-8d%s%s%s", maxID, productName, price, quantity));
+            }
+            bufferedReader.close();
+            writer.close();
         }
     }
 }

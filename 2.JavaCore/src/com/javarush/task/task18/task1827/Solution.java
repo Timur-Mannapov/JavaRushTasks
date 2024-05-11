9307fa21a17895e3fce23d187063d13e690939f4
Timur Mannapov <bla.blacarr@yandex.ru>
2024-05-11 22:25:52 +0300

    add Solution JavaCore task 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -8,21 +8,19 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            String fileName = bufferedReader.readLine();
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            FileWriter writer = new FileWriter(fileName, true);
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = console.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
             int maxID = 0;
             String str;
             while (reader.ready()) {
                 str = reader.readLine();
-                int temp = Integer.parseInt(str.substring(0,8).trim());
+                int temp = Integer.parseInt(str.substring(0, 8).trim());
                 if (maxID < temp) {
                     maxID = temp;
                 }
             }
-            reader.close();
-
             if (args.length > 0 && args[0].equals("-c")) {
                 String productName = String.format("%-30s", args[1]);
                 String price = String.format("%-8s", args[2]);
@@ -30,8 +28,6 @@ public class Solution {
                 maxID++;
                 writer.write(String.format("\n%-8d%s%s%s", maxID, productName, price, quantity));
             }
-            bufferedReader.close();
-            writer.close();
         }
     }
 }

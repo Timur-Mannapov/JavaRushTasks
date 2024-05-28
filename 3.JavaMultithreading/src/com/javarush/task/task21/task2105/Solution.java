e8d7883e8b28d9a6b3bc3d2b7689370bcf4e488d
Timur Mannapov <bla.blacarr@yandex.ru>
2024-05-28 22:36:25 +0300

    add Solution Multithreading task 2105

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Solution) || o == null) {
+            return false;
+        }
+        Solution n = (Solution) o;
+        if (!Objects.equals(first, n.first)) {
+            return false;
+        }
+        return Objects.equals(last,n.last);
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((last == null) ? 0 : last.hashCode());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}

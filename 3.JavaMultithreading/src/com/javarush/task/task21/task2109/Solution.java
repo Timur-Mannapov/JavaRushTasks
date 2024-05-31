6b92d24725a03f1f6b4b35eb9fe69ec61b5be397
Timur Mannapov <bla.blacarr@yandex.ru>
2024-05-31 22:00:22 +0300

    add Solution Multithreading task 2109

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        public Object clone() throws CloneNotSupportedException {
+            try {
+                return super.clone();
+            } catch (CloneNotSupportedException ignore) {
+
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}

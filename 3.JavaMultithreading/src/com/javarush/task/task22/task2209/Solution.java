a110bd1ef04528382de75eceb77bbc21efea7b6f
Timur Mannapov <bla.blacarr@yandex.ru>
2024-06-15 20:37:29 +0300

    add Solution Multithreading task 2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,10 +1,8 @@
 package com.javarush.task.task22.task2209;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /* 
 Составить цепочку слов
@@ -23,42 +21,45 @@ public class Solution {
         StringBuilder result = getLine(strings);
         System.out.println(result.toString());
     }
+
     //C:\Prog\JavaRushTasks\3.JavaMultithreading\src\com\javarush\task\task22\task2209\test
     public static StringBuilder getLine(String... words) {
         if (words.length == 0) {
-            return new StringBuilder("");
+            return new StringBuilder();
         }
-        List<String> list = new ArrayList<>(Arrays.asList(words));
-        int size = list.size();
-        StringBuilder builder = new StringBuilder(list.get(0));
-        for (int j = 1; j < size; j++) {
-            for (int i = 1; i < list.size(); i++) {
-                String firstStart;
-                if (builder.toString().contains(" ")) {
-                    firstStart = builder.substring(builder.lastIndexOf(" ") + 1, builder.lastIndexOf(" ") + 2);
-                } else {
-                    firstStart = builder.substring(0, 1);
-                }
+        List<StringBuilder> builderList = new ArrayList<>();
+        for (String word : words) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(word);
+            List<String> list = new ArrayList<>(Arrays.asList(words));
+            list.remove(word);
+            int count = 0;
+            while (!list.isEmpty()) {
+                String firstStart = builder.substring(0, 1);
                 String firstEnd = builder.substring(builder.length() - 1);
-                String second = list.get(i);
+                String second = list.get(0);
                 String secondStart = second.substring(0, 1);
                 String secondEnd = second.substring(second.length() - 1);
-                if (firstStart.equalsIgnoreCase(secondEnd) && !(builder.toString().matches(second))) {
+
+                if (firstStart.equalsIgnoreCase(secondEnd)) {
                     builder.insert(0, second + " ");
-                } else if (firstEnd.equalsIgnoreCase(secondStart) && !(builder.toString().matches(second))) {
+                    list.remove(second);
+                } else if (firstEnd.equalsIgnoreCase(secondStart)) {
                     builder.append(" ").append(second);
-                } else if (builder.toString().matches(second)) {
                     list.remove(second);
                 } else {
                     list.remove(second);
                     list.add(second);
+                    count++;
+                    if (count > 1000) {
+                        break;
+                    }
                 }
             }
-
+            builderList.add(builder);
         }
-
-        return builder;
-
+        return builderList.stream().max(Comparator.comparingInt(StringBuilder::length)).get();
     }
+
 }
 

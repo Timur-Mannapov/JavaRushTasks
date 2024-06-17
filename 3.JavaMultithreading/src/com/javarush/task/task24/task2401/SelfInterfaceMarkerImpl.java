b90188446ca574d497b1ff7fa87d2cb3ca50b23c
Timur Mannapov <bla.blacarr@yandex.ru>
2024-06-17 21:43:06 +0300

    add Solution Multithreading task 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}

c6778176ef7db971ce4837a5c9717e5ffe5add06
Timur Mannapov <bla.blacarr@yandex.ru>
2024-08-03 21:54:06 +0300

    add Solution Multithreading 2510

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+public class Solution extends Thread {
+
+    public Solution() {
+        UncaughtExceptionHandler handler;
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
